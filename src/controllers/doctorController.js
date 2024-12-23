fc5d58d024f8a4af4c943fa43ee8c44bd8c737ff
Azuk03 <meocuptai2@gmail.com>
2024-12-23 21:16:22 +0700

    doctor Markdown

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -16,6 +16,48 @@ let getTopDoctorHome = async (req,res) => {
     }
 }
 
+let getAllDoctors = async (req,res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors);
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server",
+        })
+    }
+}
+
+let postInfoDoctors = async (req,res) => {
+    try {
+        let response = await doctorService.saveDetailInfoDoctor(req.body);
+        return res.status(200).json(response);
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            error: -1,
+            errMessage: "Error from server"
+        })
+    }
+}
+
+let getDetailDoctorById = async (req,res) => {
+    try {
+        let infor = await doctorService.getDetailDoctorByIdService(req.query.id);
+        return res.status(200).json(infor)
+    } catch (error) {
+        console.log(e);
+        res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    postInfoDoctors: postInfoDoctors,
+    getDetailDoctorById: getDetailDoctorById,
 }
